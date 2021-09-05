5214de63c27c3b121476a43121a1ed3171fd2c99
melbarth <wizardway67@msn.com>
2021-09-05 10:24:23 -0600

    script.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+//This is a program to generate a random message each time it is opened
+
+let arr = ['Hello', 'World', 'This', 'Is', 'A', 'Test'];
+
+console.log(arr[Math.floor(Math.random() * arr.length)]);
