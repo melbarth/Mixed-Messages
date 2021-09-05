3b20f59b911fc97e022b09d1b11be66bda00a3b7
melbarth <38773166+melbarth@users.noreply.github.com>
2021-09-05 12:59:16 -0600

    Sayings into array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //This is a program to generate a random message each time it is opened
 
-let arr = ['Hello', 'World', 'This', 'Is', 'A', 'Test'];
+let arr = ['The love of your life is right in front of your eyes.', 'Behind this fortune is the love of my life', 'You have a secret admirer.', 'Love, because it is the only true adventure.', 'The love of your life will appear in front of you unexpectedly!', 'An old love will come back to you.', 'Your love life will soon be happy and harmonious.', 'Follow what calls you.', 'You’re intoxicating when you do what you love.', 'Be passionate and totally worth the chaos.', 'Do what you love. The rest will fall into place.'];
 
 console.log(arr[Math.floor(Math.random() * arr.length)]);
